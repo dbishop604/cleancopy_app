c5a9ae3a579df85a1569770c5f8053c027691f39
dbishop604 <dbishop604@gmail.com>
2025-09-14 12:42:05 -0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -2,17 +2,14 @@ import os
 import sys
 import redis
 from rq import Connection, Worker
-from processor import process_file
 
-# Ensure the app directory is on sys.path
+# Add processor to path
 sys.path.append(os.path.dirname(__file__))
 
 listen = ['default']
-
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 conn = redis.from_url(redis_url)
 
-# Define a worker that will run process_file jobs
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(str, listen))
